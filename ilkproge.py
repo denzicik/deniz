4b6cb8ae3394fefc5b8714a854e3d1e657f8aee4
denzicik <135058100+denzicik@users.noreply.github.com>
2023-05-30 21:23:16 +0300

    Create ilkproge.py
    gzl kod

---
+++ ilkproge.py
@@ -0,0 +1,8 @@
+modern_kelime_sozlu = {"asdfhsdg": "gulme" , "BB": "bay bay"}
+
+cevap = input("kelime yaz")
+
+if cevap in modern_kelime_sozlu:
+    print("bay bay")
+else:
+    print("bu sozck yok")
